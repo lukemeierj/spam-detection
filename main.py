669f0365cbc45eedb44f4be972399d9b53c14663
Luke Meier <lukemeierj@gmail.com>
2019-04-09 20:25:26 -0400

    Do Naive Bayes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import tensorflow
-import pandas
+import pandas as pd
 import numpy
+import naive_bayes
 
 def main():
-    print("It works!")
+    df = pd.read_csv("./spambase/spambase.data")
+
+    target = df["spam"].values
+    data = df.drop("spam", 1).values
+    
+    naive_bayes.runNBs(data, target)
+
 
 if __name__ == "__main__":
     main()
---
+++ naive_bayes.py
@@ -0,0 +1,30 @@
+from sklearn.naive_bayes import GaussianNB, MultinomialNB
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+def runNBs(features, labels):
+    classifiers = [
+                ("Guassian", GaussianNB()), 
+                ("Multinomial", MultinomialNB())
+              ]
+
+    for name, nb in classifiers:
+        print("Running with {} Naive Bayes".format(name))
+        runNB(nb, features, labels, scoringFns=[accuracy_score, precision_score, recall_score])
+        print()
+
+
+def runNB(nb, features, labels, scoringFns=[accuracy_score]):
+    feature_train, feature_test, label_train, label_test = train_test_split(features, labels, random_state=6)
+
+    # Fit
+    nb.fit(feature_train, label_train)
+
+    # Predict
+    label_pred = nb.predict(feature_test)
+
+    # Accuracy
+    for fn in scoringFns:
+        print("{}: {}".format(fn.__name__, fn(label_test, label_pred)))
