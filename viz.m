2e2d5acecd70d43346409eb9b4d4a8555a620025
Luke Meier <lukemeierj@gmail.com>
2019-05-01 13:39:23 -0400

    Add confidence scores

--- viz.m
+++ viz.m
@@ -1,6 +1,8 @@
 table = readtable('spambase/spambase.dat'); 
 data = table2array(table);
 
+freq_bins = logspace(log2(0.000000001), log2(100), 7);  
+
 % all word freq 
 
 histogram(data(:, 1:48));
@@ -16,7 +18,7 @@ saveas(gcf,[pwd '/img/word_freq.png']);
 dat = reshape(data(:, 1:48).',1,[]);
 histogram(dat(dat > 1), 50);
 
-title("All 48 word frequency columns, outliers");
+title("All 48 word frequency columns (values > 1)");
 xlabel("Frequency");
 ylabel("Frequency of this frequency");
 
@@ -37,7 +39,7 @@ histogram(dat(dat > 1), 50);
 
 % all char freq outliers
 
-title("All 6 char frequency columns, outliers");
+title("All 6 char frequency columns (values > 1)");
 xlabel("Frequency");
 ylabel("Frequency of this frequency");
 
@@ -56,7 +58,7 @@ saveas(gcf,[pwd '/img/all_counts.png']);
 dat = reshape(data(:, 55:57).',1,[]);
 histogram(dat(dat > 2000), 50);
 
-title("Capital run-length counts/avgs, outliers");
+title("Capital run-length counts/avgs (values > 2000)");
 xlabel("Count");
 ylabel("Frequency");
 
